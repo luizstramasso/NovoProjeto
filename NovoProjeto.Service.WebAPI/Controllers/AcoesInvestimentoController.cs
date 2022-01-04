c41eca3d8547262eeaf4cc243d637353565a35c0
luizstramasso <Las@0818>
2022-01-04 02:44:33 -0300

    Criação do projeto da api e controller de ações de investimentos

---
+++ NovoProjeto.Service.WebAPI/Controllers/AcoesInvestimentoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NovoProjeto.Domain.Interface.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NovoProjeto.Service.WebAPI.Controllers
+{
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class AcoesInvestimentoController : ControllerBase
+    {
+        private readonly IFinanceInfraService _financeInfraService;
+
+        public AcoesInvestimentoController( IFinanceInfraService financeInfraService )
+        {
+            _financeInfraService = financeInfraService;
+        }
+
+        // GET: lista de acoes disponíveis
+        ///<summary>
+        ///</summary>
+        ///<remarks>
+        ///</remarks>
+        [HttpGet]
+        [Route( "listarAcoes" )]
+        public async Task<IActionResult> ListarAcoes()
+        {
+            var result = await _financeInfraService.ListarAcoes();
+
+            return Ok( result );
+        }
+    }
+}
