d25cb5ca7bc6b59146a58f97c0e1facc678b7b6f
luizstramasso <Las@0818>
2022-01-05 22:42:56 -0300

    Alteração de nomenclatura de classes de serviços de infra, suas interfaces e organização de Name Spaces para melhor entendimento do código

---
+++ NovoProjeto.Domain/Interface/Service/RequestService/IFinanceRequestService.cs
@@ -0,0 +1,10 @@
+﻿using NovoProjeto.Infra.CrossCutting.Util.ServiceEntity;
+using System.Threading.Tasks;
+
+namespace NovoProjeto.Domain.Interface.Service.RequestService
+{
+    public interface IFinanceRequestService
+    {
+        Task<AcoesDisponiveisServiceEntity> ListarAcoesDisponiveis();
+    }
+}
--- NovoProjeto.Infra.Service/RequestService/FinanceRequestService.cs
+++ NovoProjeto.Infra.Service/RequestService/FinanceRequestService.cs
@@ -1,33 +1,34 @@
 ﻿using Newtonsoft.Json;
-using NovoProjeto.Domain.Interface.Service;
-using NovoProjeto.Infra.CrossCutting.Util.DataEntity;
+using NovoProjeto.Domain.Interface.Service.RequestService;
 using NovoProjeto.Infra.CrossCutting.Util.HelpEntity;
+using NovoProjeto.Infra.CrossCutting.Util.ServiceEntity;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace NovoProjeto.Infra.Service.Service
+namespace NovoProjeto.Infra.Service.RequestService
 {
-    public class FinanceInfraService : IFinanceInfraService
+    public class FinanceRequestService : IFinanceRequestService
     {
         private readonly IHttpClientFactory _clientFactory;
 
-        public FinanceInfraService( IHttpClientFactory clientFactory )
+        public FinanceRequestService( IHttpClientFactory clientFactory )
         {
             _clientFactory = clientFactory;
         }
 
-        public async Task<List<AcaoInvestimentoDataEntity>> ListarAcoes()
+        public async Task<AcoesDisponiveisServiceEntity> ListarAcoesDisponiveis()
         {
-            var listaAcoes = new List<AcaoInvestimentoDataEntity>();
+            var acoesDisponiveis = new AcoesDisponiveisServiceEntity();
             var endpoint = @"https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=AAPL%2CAMZN%2CGOOG%2CFB%2CTSLA%2CNFLX%2CIBM";
             var request = new HttpRequestMessage( HttpMethod.Get, endpoint );
             request.Headers.Add( "accept", " application/json" );
             request.Headers.Add( "X-API-KEY", "OzeRJXB0Gsa4Qk90oCtrG92AUlqqVSUPaTjgDFVM" );
 
             var client = _clientFactory.CreateClient();
+
             try
             {
 
@@ -36,11 +37,12 @@ namespace NovoProjeto.Infra.Service.Service
                 if( response.IsSuccessStatusCode )
                 {
                     var responseString = await response.Content.ReadAsStringAsync();
-                    var rootObj = JsonConvert.DeserializeObject<ResponseYahooEntity>( responseString );
+                    var rootObj = JsonConvert.DeserializeObject<ResponseYahooHelpEntity>( responseString );
+                    var listaAcoes = new List<AcaoDisponivelServiceEntity>();
 
                     foreach( var item in rootObj.QuoteResponse.Result )
                     {
-                        listaAcoes.Add( new AcaoInvestimentoDataEntity()
+                        listaAcoes.Add( new AcaoDisponivelServiceEntity()
                         {
                             CodigoAcao = item.Symbol,
                             PrecoAcao = item.RegularMarketPrice,
@@ -48,17 +50,27 @@ namespace NovoProjeto.Infra.Service.Service
                             MoedaCorrente = item.Currency
                         } );
                     }
+
+                    if( listaAcoes.Any() )
+                    {
+                        acoesDisponiveis.AcoesDisponiveis = listaAcoes;
+                    }
+                }
+                else
+                {
+                    acoesDisponiveis.Erros = new List<ErroHelpEntity>() { new ErroHelpEntity( "Erro ao solicitar ações disponíveis. Status de requisição: " + response.StatusCode.ToString(), this.GetType() ) };
                 }
             }
-            catch( Exception )
+            catch( Exception ex )
             {
-
-                throw;
+                acoesDisponiveis.Erros = new List<ErroHelpEntity>() { new ErroHelpEntity( ex.Message, this.GetType() ) };
+            }
+            finally
+            {
+                client.Dispose();
             }
 
-            return listaAcoes;
+            return acoesDisponiveis;
         }
     }
-
-
 }
