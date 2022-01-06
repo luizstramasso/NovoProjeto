ac605e080e17fd90efeb562a46e40a2fd629db46
luizstramasso <Las@0818>
2022-01-05 22:38:02 -0300

    Adição de funcionalidades Swagger

--- NovoProjeto.Service.WebAPI/Startup.cs
+++ NovoProjeto.Service.WebAPI/Startup.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.PlatformAbstractions;
 using Microsoft.OpenApi.Models;
 using NovoProjeto.Infra.CrossCutting.IoC;
+using System;
+using System.IO;
 
 namespace NovoProjeto.Service.WebAPI
 {
@@ -23,9 +26,25 @@ namespace NovoProjeto.Service.WebAPI
             services.NovoProjetoSetup( typeof( Startup ) );
             services.AddControllers();
             services.AddSwaggerGen( c =>
-             {
-                 c.SwaggerDoc( "v1", new OpenApiInfo { Title = "NovoProjeto", Version = "v1" } );
-             } );
+            {
+                c.SwaggerDoc( "v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "NovoProjeto API",
+                    Description = "Projeto de portifólio, usando DDD, Repository, IoC, Swagger, entre outros",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Luiz Stramasso",
+                        Email = "luizstramasso@gmail.com",
+                        Url = new Uri( "https://luizstramasso.github.io/" ),
+                    }
+                } );
+                var appPath = PlatformServices.Default.Application.ApplicationBasePath;
+                var appName = PlatformServices.Default.Application.ApplicationName;
+                var xmlPath = Path.Combine( appPath, $"{appName}.xml" );
+
+                c.IncludeXmlComments( xmlPath );
+            } );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
