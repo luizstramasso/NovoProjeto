8765be749a23071f0eaa5924506ffc6740fc9070
luizstramasso <Las@0818>
2022-01-10 02:42:51 -0300

    Criação de classes de input para melhor utilização das classes em requisições HTTP

---
+++ NovoProjeto.Infra.CrossCutting.Util/ViewEntity/Input/AcaoInvestimentoInput.cs
@@ -0,0 +1,8 @@
+﻿namespace NovoProjeto.Infra.CrossCutting.Util.ViewEntity.Input
+{
+    public class AcaoInvestimentoInput
+    {
+        public string CodigoAcao { get; set; }
+        public string RazaoSocial { get; set; }
+    }
+}
---
+++ NovoProjeto.Infra.CrossCutting.Util/ViewEntity/Input/OperacaoInvestimentoInput.cs
@@ -0,0 +1,9 @@
+﻿namespace NovoProjeto.Infra.CrossCutting.Util.ViewEntity.Input
+{
+    public class OperacaoInvestimentoInput
+    {
+        public string CodigoAcao { get; set; }
+        public int QuantidadeAcoes { get; set; }
+        public string TipoOperacao { get; set; }
+    }
+}
