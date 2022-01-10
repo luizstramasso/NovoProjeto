28efc918592f8f4de75e69df6895862557a83211
luizstramasso <Las@0818>
2022-01-10 02:43:30 -0300

    Criação das classes de output, para melhor tratamento de respostas as requisições HTTP (a ser aprimorado)

---
+++ NovoProjeto.Infra.CrossCutting.Util/ViewEntity/Output/OperacaoInvestimentoOutput.cs
@@ -0,0 +1,12 @@
+﻿namespace NovoProjeto.Infra.CrossCutting.Util.ViewEntity.Output
+{
+    public class OperacaoInvestimentoOutput : BaseOutput
+    {
+        public string Codigo { get; set; }
+        public string RazaoSocial { get; set; }
+        public string TipoOperacao { get; set; }
+        public double ValorAcao { get; set; }
+        public int Quantidade { get; set; }
+        public decimal ValorTotalOperacao { get; set; }
+    }
+}
